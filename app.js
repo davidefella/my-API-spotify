9787517d40e4b010b6c7c313b14027ce479eb339
Davide <davidefella@users.noreply.github.com>
2023-05-06 00:46:29 +0200

    Refactor

--- app.js
+++ app.js
@@ -3,27 +3,24 @@ require("dotenv").config();
 
 const express = require("express");
 
-/* querystring può essere sostituito utilizzando il metodo encodeURIComponent, ad esempio
- * 'access_token=' + encodeURIComponent(access_token) + '&refresh_token=' + encodeURIComponent(refresh_token);
- *
- * L'encoding mi serve se nella mia URL ho caratteri che possono crearmi problemi come &
- */
 const querystring = require("querystring");
 const cookieParser = require("cookie-parser");
 const axios = require("axios");
 const client_secret_base64 = Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')
 
 let app = express();
-let stateKey = "spotify_auth_state"; // name of the cookie
 
-/** Il cookie mi serve perchè devo memorizzare il mio state (random string) */
+let stateKey = "spotify_auth_state"; 
+let access_token = null; 
+let refresh_token = null; 
+
 app.use(express.static(__dirname + "/public")).use(cookieParser());
 
 app.get("/login", function (req, res) {
 
-  let state = generateRandomString(16);
+  let spotifyAuthState = generateRandomString(16);
 
-  res.cookie(stateKey, state); // set cookie to travel with request
+  res.cookie(stateKey, spotifyAuthState);
 
   const scope = "user-read-private user-read-email user-top-read";
   res.redirect(
@@ -33,7 +30,7 @@ app.get("/login", function (req, res) {
         client_id: process.env.CLIENT_ID,
         scope: scope,
         redirect_uri: process.env.REDIRECT_URI,
-        state: state,
+        state: spotifyAuthState,
       })
   );
 });
@@ -43,14 +40,12 @@ app.get("/callback", function (req, res) {
   let state = req.query.state || null;
   let storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  // TODO: Implementare un meccanismo di gestione dell'errore
   if (state === null || state !== storedState) {
     const query = querystring.stringify({ request_error: "state_mismatch" });
 
     res.redirect("/error?" + query);
   } else {
-    //Resetto il cookie per il prossimo giro
-    res.clearCookie(stateKey); // eat (clear) cookie
+    res.clearCookie(stateKey); 
 
     axios({
       url: 'https://accounts.spotify.com/api/token',
@@ -67,31 +62,10 @@ app.get("/callback", function (req, res) {
     })
     .then(response => {
       if (response.status === 200) {
-        let access_token = response.data.access_token;
-        let refresh_token = response.data.refresh_token;
+        access_token = response.data.access_token;
+        refresh_token = response.data.refresh_token;
   
         res.redirect('/#' + querystring.stringify({ access_token: access_token, refresh_token: refresh_token }));
-  
-        axios({
-          url: 'https://api.spotify.com/v1/me/top/artists',
-          method: 'GET',
-          headers: {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'},
-          params: {'time_range': 'short_term', 'limit': 3}
-        })
-        .then(function (response) {
-          console.log(response.data.items); // visualizza la lista di artisti
-
-          const topArtistsDiv = document.getElementById("top-artists");
-          const topArtists = response.data.items;
-          topArtists.forEach(function(artist) {
-            const artistName = document.createElement("p");
-            artistName.textContent = artist.name;
-            topArtistsDiv.appendChild(artistName);
-          });
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
 
       } else {
         console.log("Errore gestione status");
@@ -102,6 +76,42 @@ app.get("/callback", function (req, res) {
     });
   }
 });
+
+app.get("/topartists", function (req, res) {
+  console.log("Access token: " + access_token); 
+
+  if (access_token === null) {
+    const query = querystring.stringify({ request_error: "state_mismatch" });
+
+    res.redirect("/error?" + query);
+  } else {
+
+    axios({
+      url: 'https://api.spotify.com/v1/me/top/artists',
+      method: 'GET',
+      headers: {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'},
+      params: {'time_range': 'short_term', 'limit': 3}
+    })
+    .then(function (response) {
+      let artists = []; 
+
+      response.data.items.forEach(element => {
+        artists.push(element.name); 
+     }); 
+
+     res.json(artists);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+});
+
+
+
+
+
+
 
 // AUX methods
 function generateRandomString(length) {
