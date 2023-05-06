ac0ce8d6493b97fcc13ee926268fad0ccab024ac
Davide <davidefella@users.noreply.github.com>
2023-05-06 16:55:04 +0200

    Added constants

--- app.js
+++ app.js
@@ -1,98 +1,59 @@
 // https://github.com/Kickblip/spotify-auth-code-example
 require("dotenv").config();
 
-const { authorize } = require('./server/auth');
-
+const { authorize, callback } = require("./server/auth");
 
 const express = require("express");
 
 const querystring = require("querystring");
 const cookieParser = require("cookie-parser");
 const axios = require("axios");
-const client_secret_base64 = Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')
+
+const { COOKIE_AUTH_TOKEN } = require("./utils/constants");
 
 let app = express();
-
-let stateKey = "spotify_auth_state"; 
-let access_token = null; 
-let refresh_token = null; 
 
 app.use(express.static(__dirname + "/public")).use(cookieParser());
 
 app.get("/login", function (req, res) {
-
-  authorize(res, stateKey); 
+  authorize(res);
 });
 
 app.get("/callback", function (req, res) {
-  let code = req.query.code || null;
-  let state = req.query.state || null;
-  let storedState = req.cookies ? req.cookies[stateKey] : null;
-
-  if (state === null || state !== storedState) {
-    const query = querystring.stringify({ request_error: "state_mismatch" });
-
-    res.redirect("/error?" + query);
-  } else {
-    res.clearCookie(stateKey); 
-
-    axios({
-      url: 'https://accounts.spotify.com/api/token',
-      method: 'post',
-      params: {
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: process.env.REDIRECT_URI
-      },
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + client_secret_base64
-      }
-    })
-    .then(response => {
-      if (response.status === 200) {
-        access_token = response.data.access_token;
-        refresh_token = response.data.refresh_token;
-  
-        res.redirect('/#' + querystring.stringify({ access_token: access_token, refresh_token: refresh_token }));
-
-      } else {
-        console.log("Errore gestione status");
-      }
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  }
+  callback(req, res);
 });
 
 app.get("/topartists", function (req, res) {
-  console.log("Access token: " + access_token); 
+  let access_token = req.cookies ? req.cookies[COOKIE_AUTH_TOKEN] : null;
+
+  console.log("Access token: " + access_token);
 
   if (access_token === null) {
     const query = querystring.stringify({ request_error: "state_mismatch" });
 
     res.redirect("/error?" + query);
   } else {
-
     axios({
-      url: 'https://api.spotify.com/v1/me/top/artists',
-      method: 'GET',
-      headers: {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'},
-      params: {'time_range': 'short_term', 'limit': 3}
+      url: "https://api.spotify.com/v1/me/top/artists",
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + access_token,
+        "Content-Type": "application/json",
+      },
+      params: { time_range: "short_term", limit: 3 },
     })
-    .then(function (response) {
-      let artists = []; 
+      .then(function (response) {
+        let artists = [];
 
-      response.data.items.forEach(element => {
-        artists.push(element.name); 
-     }); 
+        response.data.items.forEach((element) => {
+          artists.push(element.name);
+        });
 
-     res.json(artists);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+        res.json(artists);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 });
 
--- server/auth.js
+++ server/auth.js
@@ -1,11 +1,15 @@
 const querystring = require("querystring");
+const axios = require("axios");
 
 const { generateRandomString } = require('../utils/state-generator');
+const { COOKIE_SPOTIFY_AUTH_STATE, COOKIE_AUTH_TOKEN, COOKIE_REFRESH_TOKEN } = require('../utils/constants');
 
-function authorize(res, stateKey){
+const client_secret_base64 = Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'); 
+
+function authorize(res){
     let spotifyAuthState = generateRandomString(16);
 
-    res.cookie(stateKey, spotifyAuthState);
+    res.cookie(COOKIE_SPOTIFY_AUTH_STATE, spotifyAuthState);
 
     const scope = "user-read-private user-read-email user-top-read";
 
@@ -21,4 +25,48 @@ function authorize(res, stateKey){
     );
 }
 
-module.exports = { authorize };
+function callback(req, res ){
+    let code = req.query.code || null;
+    let state = req.query.state || null;
+    let storedState = req.cookies ? req.cookies[COOKIE_SPOTIFY_AUTH_STATE] : null;
+    
+    console.log('In callback calling'); 
+
+    if (state === null || state !== storedState) {
+      const query = querystring.stringify({ request_error: "state_mismatch" });
+  
+      res.redirect("/error?" + query);
+    } else {
+      res.clearCookie(COOKIE_SPOTIFY_AUTH_STATE); 
+  
+      axios({
+        url: 'https://accounts.spotify.com/api/token',
+        method: 'post',
+        params: {
+          grant_type: 'authorization_code',
+          code: code,
+          redirect_uri: process.env.REDIRECT_URI
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + client_secret_base64
+        }
+      })
+      .then(response => {
+        if (response.status === 200) {
+          res.cookie(COOKIE_AUTH_TOKEN, response.data.access_token);
+          res.cookie(COOKIE_REFRESH_TOKEN, response.data.refresh_token);
+    
+          res.redirect('/#' + querystring.stringify({ access_token: response.data.access_token, refresh_token: response.data.refresh_token }));
+  
+        } else {
+          console.log("Errore gestione status");
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+}
+
+module.exports = { authorize, callback };
